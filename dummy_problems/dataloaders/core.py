7113b11753b2fc0fa166183eace9a010578ff56f
jmh <jorge.mariscal.harana@gmail.com>
2024-12-23 23:09:14 +0000

    feat: working dataset and datamodule

--- dummy_problems/dataloaders/core.py
+++ dummy_problems/dataloaders/core.py
@@ -1,25 +1,38 @@
 from pathlib import Path
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torchvision.datasets import VisionDataset
+from torchvision.io import decode_image, ImageReadMode
+from torchvision.transforms import v2
 import lightning as L
 from typing import Tuple, Any
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
-from torchvision.io import read_image
+
+DEFAULT_TRANSFORM = v2.Compose([v2.ToDtype(torch.float32, scale=True)])
+
+def count_target_frequency(dataloader) -> dict:
+    targets = sorted([x.item() for xs in dataloader() for x in xs[1]])
+    print(pd.DataFrame(targets).value_counts().to_string(header=False))
+
 
 class LettersDataset(VisionDataset):
     def __init__(self, root, transform=None):
         super().__init__(root, transform=transform)
-        self.__get_image_dirs()
+        self.__load_data()
 
-    def __get_image_dirs(self):
+    def __load_data(self):
         if not self.root.exists():
             raise RuntimeError(f"Dataset not found at {self.root}.")
 
-        self.image_dirs = sorted([f for f in self.root.iterdir()])
-        
+        self.image_dirs = sorted([d for d in self.root.iterdir()])
+        labels = [label.stem[0] for label in self.image_dirs]
+        self.labels_to_targets = {l: t for t, l in enumerate(sorted(set(labels)))}
+        self.targets = torch.tensor([self.labels_to_targets[l] for l in labels])
+    
     def __getitem__(self, idx: int) -> Tuple[Any, Any]:
-        img = read_image(self.image_dirs[idx])
-        target = self.image_dirs[idx].stem[0]
+        img = decode_image(self.image_dirs[idx], mode=ImageReadMode.RGB)
+        target = self.targets[idx]
 
         if self.transform is not None:
             img = self.transform(img)
@@ -44,12 +57,10 @@ class LettersDataModule(L.LightningDataModule):
 
     def setup(self, stage: str):
         if stage == "fit":
-            dataset = LettersDataset(self.dataset_dir / "train")
-            split = [0.8, 0.2]
-            generator = torch.Generator().manual_seed(42)
-            self.letters_train, self.letters_val = random_split(dataset, split, generator=generator)
+            dataset = LettersDataset(self.dataset_dir / "train", transform=DEFAULT_TRANSFORM)
+            self.letters_train, self.letters_val = train_test_split(dataset, test_size=0.2, random_state=42, shuffle=True, stratify=dataset.targets)
         elif stage == "test":
-            self.letters_test = LettersDataset(self.dataset_dir / "test")
+            self.letters_test = LettersDataset(self.dataset_dir / "test", transform=DEFAULT_TRANSFORM)
         else:
             raise NotImplementedError(f"{stage} stage not implemented")
 
@@ -74,4 +85,14 @@ if __name__ == "__main__":
     data = LettersDataModule(settings)
     data.setup("fit")
     data.setup("test")
-    print("DataModule check successful")
+    
+    print("Training split size:", len(data.train_dataloader()))
+    print("Validation split size:", len(data.val_dataloader()))
+    print("Test split size:", len(data.test_dataloader()))
+
+    print("\nTarget frequencies in the training split:")
+    count_target_frequency(data.train_dataloader)
+    print("\nTarget frequencies in the validation split:")
+    count_target_frequency(data.val_dataloader)
+    print("\nTarget frequencies in the test split:")
+    count_target_frequency(data.test_dataloader)
